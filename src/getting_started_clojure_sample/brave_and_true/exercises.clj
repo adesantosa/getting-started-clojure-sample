924ba7972809d89e240ad0f20949701d58c588a3
André dos Santos <adesantosa@gmail.com>
2022-01-17 15:56:30 -0300

    first exercise

---
+++ src/getting_started_clojure_sample/brave_and_true/exercises.clj
@@ -0,0 +1,52 @@
+(ns getting-started-clojure-sample.brave-and-true.exercises)
+
+
+(def asym-aliens-body-parts [{:name "head" :size 3}
+                             {:name "first-left-eye" :size 1}
+                             {:name "second-left-eye" :size 1}
+                             {:name "third-left-eye" :size 1}
+                             {:name "four-left-eye" :size 1}
+                             {:name "five-left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "first-shoulder" :size 3}
+                             {:name "second-shoulder" :size 3}
+                             {:name "third-shoulder" :size 3}
+                             {:name "four-shoulder" :size 3}
+                             {:name "five-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+
+
+;Create a function that’s similar to symmetrize-body-parts except that it has to work with weird space aliens
+;with radial symmetry. Instead of two eyes, arms, legs, and so on, they have five.
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"-left-" "-right-")
+   :size (:size part)})
+
+(defn better-symmetrize-body-parts-with-aliens
+  "Exercise"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts
+          ))
+
+(println (better-symmetrize-body-parts-with-aliens asym-aliens-body-parts))
+
--- src/getting_started_clojure_sample/brave_and_true/hobbit_parts.clj
+++ src/getting_started_clojure_sample/brave_and_true/hobbit_parts.clj
@@ -39,7 +39,26 @@
                (into final-body-parts
                      (set [part (matching-part part)])))))))
 
-(println (symmetrize-body-parts asym-hobbit-body-parts))
+(defn my-reduce
+  ([f initial coll]
+   (loop [result initial
+          remaining coll]
+     (if (empty? remaining)
+       result
+       (recur (f result (first remaining)) (rest remaining)))))
+  ([f [head & tail]]
+   (my-reduce f head tail)))
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
+
+(println (better-symmetrize-body-parts asym-hobbit-body-parts))
+
 
 
 (loop [iteration 0]
